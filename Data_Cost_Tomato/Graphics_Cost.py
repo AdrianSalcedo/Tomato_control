68f923f8f53eadf878a7dae3f35d745e41479896
AdrianSalcedo <avarela155@gmail.com>
2019-11-30 08:43:47 -0700

    data_cost

---
+++ Data_Cost_Tomato/Graphics_Cost.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data_one_cost = pd.read_csv("One_control_Cost.csv")
+data_two_cost = pd.read_csv("Two_Control_Cost.csv")
+data_three_cost = pd.read_csv("Three_Control_Cost.csv")
+
+size(data_one_cost)
