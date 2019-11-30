acb382848df8a556e7ec88f48ba3f4a726a82789
AdrianSalcedo <avarela155@gmail.com>
2019-11-30 08:46:03 -0700

    beamer with section separates

--- Data_Cost_Tomato/Graphics_Cost.py
+++ Data_Cost_Tomato/Graphics_Cost.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data_one_cost = pd.read_csv("One_control_Cost.csv")
-data_two_cost = pd.read_csv("Two_Control_Cost.csv")
-data_three_cost = pd.read_csv("Three_Control_Cost.csv")
+data_two_cost = pd.read_csv('Two_Control_Cost.csv')
+data_three_cost = pd.read_csv('Three_Control_Cost.csv')
 
-size(data_one_cost)
+#size(data_one_cost)
